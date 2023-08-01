5e0447f6f870f6ca24c09941093d3443d7350081
Patrick Dolan <dolanp1992@gmail.com>
2023-08-01 10:35:21 -0700

    refactor questions and music object

--- scripts.js
+++ scripts.js
@@ -1,53 +1,50 @@
 // Object to Access
-const music = {
-  "1": {
-    "genre": "Rock",
+const musicGenres = {
+  "rock": {
     "artists": {
       "1": {
         "name": "The Beatles",
-        "albums": ["Revolver", "Abbey Road", "Sgt. Pepper's Lonely Hearts Club Band"]
+        "album-names": ["Revolver", "Abbey Road", "Sgt. Pepper's Lonely Hearts Club Band"]
       },
       "2": {
         "name": "Led Zeppelin",
-        "albums": ["IV", "Physical Graffiti", "Led Zeppelin II"]
+        "album-names": ["IV", "Physical Graffiti", "Led Zeppelin II"]
       },
       "3": {
         "name": "Queen",
-        "albums": ["A Night at the Opera", "The Game", "News of the World"]
+        "album-names": ["A Night at the Opera", "The Game", "News of the World"]
       }
     }
   },
-  "2": {
-    "genre": "Jazz",
+  "jazz": {
     "artists": {
       "1": {
         "name": "Miles Davis",
-        "albums": ["Kind of Blue", "Bitches Brew", "In a Silent Way"]
+        "album-names": ["Kind of Blue", "Bitches Brew", "In a Silent Way"]
       },
       "2": {
         "name": "John Coltrane",
-        "albums": ["A Love Supreme", "Giant Steps", "Blue Train"]
+        "album-names": ["A Love Supreme", "Giant Steps", "Blue Train"]
       },
       "3": {
         "name": "Ella Fitzgerald",
-        "albums": ["Ella Fitzgerald Sings the Cole Porter Song Book", "Ella and Louis", "Ella Fitzgerald Sings the Duke Ellington Song Book"]
+        "album-names": ["Ella Fitzgerald Sings the Cole Porter Song Book", "Ella and Louis", "Ella Fitzgerald Sings the Duke Ellington Song Book"]
       }
     }
   },
-  "3": {
-    "genre": "Pop",
+  "pop": {
     "artists": {
       "1": {
         "name": "Michael Jackson",
-        "albums": ["Thriller", "Bad", "Off the Wall"]
+        "album-names": ["Thriller", "Bad", "Off the Wall"]
       },
       "2": {
         "name": "Madonna",
-        "albums": ["Like a Prayer", "Ray of Light", "True Blue"]
+        "album-names": ["Like a Prayer", "Ray of Light", "True Blue"]
       },
       "3": {
         "name": "Justin Timberlake",
-        "albums": ["FutureSex/LoveSounds", "The 20/20 Experience", "Justified"]
+        "album-names": ["FutureSex/LoveSounds", "The 20/20 Experience", "Justified"]
       }
     }
   }
@@ -57,28 +54,27 @@ const music = {
 // ===================================================
 // Place the answer variables in console logs as an answer to the question.
 
+// Hint: You can access objects with square bracket notation and dot notation. 
+//       Don't forget that certain symbols and numbers in key names can't be accessed with dot notation and will need bracket notation instead. 
+
 // Log the first genre in the Music object?
-console.log("Log the first genre in the Music object? ");
+console.log("Log the first genre object in the Music object? ");
 console.log(); // <-- Answer here
 
 // Log the artist listed third under the genre 'Jazz'?
-console.log("Log the artist listed third under the genre 'Jazz'? ");
+console.log("Log the artists name listed third under the genre 'Jazz'? ");
 console.log(); // <-- Answer here
 
-// Log the albums array associated with the artist 'Queen' in the 'Rock' genre?
-console.log("Log the albums array associated with the artist 'Queen' in the 'Rock' genre? ");
+// Log the album-names array associated with the artist 'Queen' in the 'Rock' genre?
+console.log("Log the album-names array associated with the artist 'Queen' in the 'Rock' genre? ");
 console.log(); // <-- Answer here
 
 // Log one album from 'Miles Davis' in the 'Jazz' genre.
 console.log("Log one album from 'Miles Davis' in the 'Jazz' genre. ");
 console.log(); // <-- Answer here
 
-// Log how many genres there are in the Music object?
-console.log("Log how many genres there are in the Music object?");
-console.log(); // <-- Answer here
-
-// Log the array of albums by 'Led Zeppelin' in the 'Rock' genre.
-console.log("Log the array of albums by 'Led Zeppelin' in the 'Rock' genre. ");
+// Log the array of album-names by 'Led Zeppelin' in the 'Rock' genre.
+console.log("Log the array of album-names by 'Led Zeppelin' in the 'Rock' genre. ");
 console.log(); // <-- Answer here
 
 // Log the first artist listed under the genre 'Pop'?
@@ -90,9 +86,9 @@ console.log("Log the name of an album from 'John Coltrane' in the 'Jazz' genre. 
 console.log(); // <-- Answer here
 
 // Log how many artists there are in the 'Rock' genre?
-console.log("Log how many artists there are in the 'Rock' genre? ");
+console.log("Log how many albums there are in 'The Beatles' albums array? ");
 console.log(); // <-- Answer here
 
-// Log two albums by 'Michael Jackson' in the 'Pop' genre.
-console.log("Log two albums by 'Michael Jackson' in the 'Pop' genre. ");
+// Log two album-names by 'Michael Jackson' in the 'Pop' genre.
+console.log("Log two album-names by 'Michael Jackson' in the 'Pop' genre. ");
 console.log(); // <-- Answer here
