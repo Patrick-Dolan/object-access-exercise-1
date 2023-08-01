b89952de21333faa1c8a34d5cac013698e889cd6
Patrick Dolan <dolanp1992@gmail.com>
2023-08-01 09:23:24 -0700

    add basic files and starter questions

---
+++ scripts.js
@@ -0,0 +1,88 @@
+// Object to Access
+const music = {
+  "1": {
+    "genre": "Rock",
+    "artists": {
+      "1": {
+        "name": "The Beatles",
+        "albums": ["Revolver", "Abbey Road", "Sgt. Pepper's Lonely Hearts Club Band"]
+      },
+      "2": {
+        "name": "Led Zeppelin",
+        "albums": ["IV", "Physical Graffiti", "Led Zeppelin II"]
+      },
+      "3": {
+        "name": "Queen",
+        "albums": ["A Night at the Opera", "The Game", "News of the World"]
+      }
+    }
+  },
+  "2": {
+    "genre": "Jazz",
+    "artists": {
+      "1": {
+        "name": "Miles Davis",
+        "albums": ["Kind of Blue", "Bitches Brew", "In a Silent Way"]
+      },
+      "2": {
+        "name": "John Coltrane",
+        "albums": ["A Love Supreme", "Giant Steps", "Blue Train"]
+      },
+      "3": {
+        "name": "Ella Fitzgerald",
+        "albums": ["Ella Fitzgerald Sings the Cole Porter Song Book", "Ella and Louis", "Ella Fitzgerald Sings the Duke Ellington Song Book"]
+      }
+    }
+  },
+  "3": {
+    "genre": "Pop",
+    "artists": {
+      "1": {
+        "name": "Michael Jackson",
+        "albums": ["Thriller", "Bad", "Off the Wall"]
+      },
+      "2": {
+        "name": "Madonna",
+        "albums": ["Like a Prayer", "Ray of Light", "True Blue"]
+      },
+      "3": {
+        "name": "Justin Timberlake",
+        "albums": ["FutureSex/LoveSounds", "The 20/20 Experience", "Justified"]
+      }
+    }
+  }
+}
+
+// Console log the following answers to the questions:
+// ===================================================
+// Place the answer variables in console logs as an answer to the question.
+
+// What is the first genre in the Music object?
+console.log("What is the first genre in the Music object? ");
+
+// Which artist is listed third under the genre "Jazz"?
+console.log("Which artist is listed third under the genre 'Jazz'?");
+
+// What albums are associated with the artist "Queen" in the "Rock" genre?
+console.log("What albums are associated with the artist 'Queen' in the 'Rock' genre?");
+
+// Name one album from "Miles Davis" in the "Jazz" genre.
+console.log("Name one album from 'Miles Davis' in the 'Jazz' genre.");
+
+// How many genres are there in the Music object?
+console.log("How many genres are there in the Music object?");
+
+// List all the albums by "Led Zeppelin" in the "Rock" genre.
+console.log("List all the albums by 'Led Zeppelin' in the 'Rock' genre.");
+
+// Who is the first artist listed under the genre "Pop"?
+console.log("Who is the first artist listed under the genre 'Pop'?");
+
+// Provide the name of an album from "John Coltrane" in the "Jazz" genre.
+console.log("Provide the name of an album from 'John Coltrane' in the 'Jazz' genre.");
+
+// How many artists are there in the "Rock" genre?
+console.log("How many artists are there in the 'Rock' genre?");
+
+// Mention two albums by "Michael Jackson" in the "Pop" genre.
+console.log("Mention two albums by 'Michael Jackson' in the 'Pop' genre.");
