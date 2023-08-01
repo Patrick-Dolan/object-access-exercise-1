243119beaca1a17eae1e1a9437538f1eda681719
Patrick Dolan <dolanp1992@gmail.com>
2023-08-01 11:17:03 -0700

    change number string to numbers

--- scripts.js
+++ scripts.js
@@ -3,15 +3,15 @@
 const musicGenres = {
   "rock": {
     "artists": {
-      "1": {
+      1: {
         "name": "The Beatles",
         "album-names": ["Revolver", "Abbey Road", "Sgt. Pepper's Lonely Hearts Club Band"]
       },
-      "2": {
+      2: {
         "name": "Led Zeppelin",
         "album-names": ["IV", "Physical Graffiti", "Led Zeppelin II"]
       },
-      "3": {
+      3: {
         "name": "Queen",
         "album-names": ["A Night at the Opera", "The Game", "News of the World"]
       }
@@ -19,15 +19,15 @@ const musicGenres = {
   },
   "jazz": {
     "artists": {
-      "1": {
+      1: {
         "name": "Miles Davis",
         "album-names": ["Kind of Blue", "Bitches Brew", "In a Silent Way"]
       },
-      "2": {
+      2: {
         "name": "John Coltrane",
         "album-names": ["A Love Supreme", "Giant Steps", "Blue Train"]
       },
-      "3": {
+      3: {
         "name": "Ella Fitzgerald",
         "album-names": ["Ella Fitzgerald Sings the Cole Porter Song Book", "Ella and Louis", "Ella Fitzgerald Sings the Duke Ellington Song Book"]
       }
@@ -35,15 +35,15 @@ const musicGenres = {
   },
   "pop": {
     "artists": {
-      "1": {
+      1: {
         "name": "Michael Jackson",
         "album-names": ["Thriller", "Bad", "Off the Wall"]
       },
-      "2": {
+      2: {
         "name": "Madonna",
         "album-names": ["Like a Prayer", "Ray of Light", "True Blue"]
       },
-      "3": {
+      3: {
         "name": "Justin Timberlake",
         "album-names": ["FutureSex/LoveSounds", "The 20/20 Experience", "Justified"]
       }
